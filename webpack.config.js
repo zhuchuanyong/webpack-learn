29927155998dbe5e7586ad7faded65fb61be1f91
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-01-09 21:56:42 +0800

    1-5 plugins使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './main.js',
     output: {
@@ -10,16 +11,16 @@ module.exports = {
             {
                 // 用正则去匹配要用该 loader 转换的 CSS 文件
                 test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true
-                        }
-                    }
-                ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
 };
