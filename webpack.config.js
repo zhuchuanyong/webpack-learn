5d3541a6f2809cc61ad098fd9daa5ce5d805554f
zhuchuanyong <zhuchuanyong@vtstar.net>
2020-01-09 21:46:21 +0800

    1-4 使用loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,22 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            {
+                // 用正则去匹配要用该 loader 转换的 CSS 文件
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            }
+        ]
     }
 };
